0c5690e717e3b103524e40e43dce3582684e5cf1
gimsy <gimsy1@gmail.com>
2023-10-22 14:23:57 +0200

    update E2

--- lib/python/Plugins/Extensions/OPDBoot/plugin.py
+++ lib/python/Plugins/Extensions/OPDBoot/plugin.py
@@ -577,7 +577,7 @@ class OPDBootImageChoose(Screen):
                 else:
                         menulist = []
                         menulist.append((_('Install from /media/opdboot/OPDBootUpload'), 'media'))
-                        menulist.append((_('Install from Internet (OpenOPD,OpenATV,OpenNFR,OpenVIX,OpenHDF)'), 'internet'))
+                        menulist.append((_('Install from Internet (OpenDroid,OpenATV,OpenNFR,OpenVIX,OpenHDF)'), 'internet'))
                         self.session.openWithCallback(self.menuCallback, ChoiceBox, title=_('Choose they way for installation!'), list=menulist)
 
         def menuCallback(self, choice):
