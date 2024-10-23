065b369d4346e7624b829a555b15e5bc4fcb9352
formiano <formianodicastellone@hotmail.it>
2024-10-23 18:25:06 +0200

    Update OscamSmartcard.py
    update oscamsmartcard code

--- lib/python/Screens/OscamSmartcard.py
+++ lib/python/Screens/OscamSmartcard.py
@@ -940,6 +940,7 @@ class OscamSmartcard(ConfigListScreen, Screen):
 	def showNews(self):
 		lastinfo =  ""
 		x = " : "
+		lastinfo += "22-10-2024" + x + _("update oscamsmartcard code") + "\n"
 		lastinfo += "22-10-2024" + x + _("update oscam 11845") + "\n"
 		lastinfo += "06-11-2020" + x + _("added dropbadcws on cccam import") + "\n"
 		lastinfo += "10-20-2018" + x + _("added bcm arm 64 bit CPU") + "\n"
