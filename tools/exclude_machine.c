5d9a6e60d1440185ff47a80a8c2beee92ba854c9
formiano <formiano@formiano>
2024-11-20 13:59:13 +0100

    update excluded_machines

--- tools/exclude_machine.c
+++ tools/exclude_machine.c
@@ -3,7 +3,7 @@
 // Funzione per verificare se una macchina è esclusa
 int is_machine_excluded(const char *machine_name) {
     // Elenco delle macchine escluse
-    const char *excluded_machines[] = {"lc", "sh1", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h9se", "h9combo", "h9combose", "h10", "h11", "i55", "i55se", "i55plus", "h17", "hzero"};
+    const char *excluded_machines[] = {"zgemmalc", "zgemmash1", "zgemmah3", "zgemmah4", "zgemmah5", "zgemmah6", "zgemmah7", "zgemmah8", "zgemmah9", "zgemmah9se", "zgemmah9combo", "zgemmah9combose", "zgemmah10", "zgemmah11", "zgemmai55", "zgemmai55se", "zgemmai55plus", "zgemmah17", "zgemmahzero", "zgemmah9twin"};
     size_t excluded_count = sizeof(excluded_machines) / sizeof(excluded_machines[0]);
 
     for (size_t i = 0; i < excluded_count; ++i) {
