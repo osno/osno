d477a568129c0ebe6ac13afa643cb55c215b2939
formiano <nuova-email@gmail.com>
2024-12-21 14:04:22 +0100

    update ci

--- CI/build.sh
+++ CI/build.sh
@@ -4,8 +4,8 @@
 # You're not allowed to remove my copyright or reuse this script without putting this header.
 
 setup_git() {
-  git config --global user.email "bot@formianodicastellone@hotmail.it"
-  git config --global user.name "opendroid-bot"
+  git config --global user.email "bot@tipomimmo@gmail.com"
+  git config --global user.name "osno-bot"
 }
 
 commit_files() {
@@ -13,15 +13,15 @@ commit_files() {
   rm -rf *.pyc
   rm -rf *.pyo
   rm -rf *.mo
-  git checkout pyton-3.12
+  git checkout pyton-3.13
   ./CI/chmod.sh
   ./CI/dos2unix.sh
   ./CI/PEP8.sh
 }
 
 upload_files() {
-  git remote add upstream https://${GITHUB_TOKEN}@github.com/formiano/enigma2.git > /dev/null 2>&1
-  git push --quiet upstream pyton-3.12 || echo "failed to push with error $?"
+  git remote add upstream https://${GITHUB_TOKEN}@github.com/osno/osno.git > /dev/null 2>&1
+  git push --quiet upstream pyton-3.13 || echo "failed to push with error $?"
 }
 
 setup_git
